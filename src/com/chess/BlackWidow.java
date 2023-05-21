6b11c9c7f6729914ce930827de4136016189e590
Tran Thuan <dtthuan.1211@gmail.com>
2023-05-21 15:01:31 +0700

    final ver2

---
+++ src/com/chess/BlackWidow.java
@@ -0,0 +1,11 @@
+package src.com.chess;
+
+
+import src.com.chess.gui.Table;
+
+public class BlackWidow {
+
+    public static void main(final String[] args) throws Exception {
+        Table.get().show();
+    }
+}
